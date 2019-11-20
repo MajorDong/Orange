3345bd0be11dac8f4148ae79aa1f47f56f599d2f
MajorDong <odsts@outlook.com>
2019-11-20 18:52:33 +0800

    检查o-button-group的子元素是否为button

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 import Vue from 'vue';
 import Button from './button'
 import Icon from './icon'
+import ButtonGroup from './button-group'
 
 Vue.component('o-button', Button)
 Vue.component('o-icon', Icon)
+Vue.component('o-button-group',ButtonGroup)
 
 new Vue({
   el: '#app',
---
+++ src/button-group.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="o-button-group">
+    <slot><slot>
+  </div>
+</template>
+
+<script>
+export default {
+  mounted(){
+    for(let node of this.$el.children){
+      let name = node.nodeName.toLowerCase()
+      if( name !== 'button'){
+        console.warn(`o-button-group的子元素应全是o-button，但你写的为${name}`)
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.o-button-group{
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  vertical-align: middle;
+  > .o-button {
+    border-radius: 0;
+    margin-left: -1px; //前一个border被后一个挡住了
+    &:first-child{
+      border-top-left-radius: var(--border-radius);
+      border-bottom-left-radius: var(--border-radius)
+    }
+    &:last-child{
+      border-top-right-radius: var(--border-radius);
+      border-bottom-right-radius: var(--border-radius)
+    }
+    &:hover{
+      position: relative;
+      z-index: 1;
+    }
+  }
+}
+</style>
