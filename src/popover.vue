e498089d34368d3de2a6c9e25e9ff02fd14a840f
MajorDong <odsts@outlook.com>
2019-12-21 05:16:53 +0800

    添加popover默认样式

--- src/popover.vue
+++ src/popover.vue
@@ -3,7 +3,7 @@
     <div ref="contentWrapper" class="contentWrapper" v-if="visible">
       <slot name="content"></slot>
     </div>
-    <span ref="triggerWrapper">
+    <span ref="triggerWrapper" style="display:inline-block;">
       <slot></slot>
     </span>
   </div>
@@ -69,8 +69,44 @@ export default {
 }
 .contentWrapper {
   position: absolute;
+  max-width: 20em;
+  
+  word-break: break-all;
   transform: translateY(-100%);
-  border: 1px solid red;
-  box-shadow: 0 0 3px rgba(0, 0, 0, 0.5);
+  border: 1px solid #333;
+  border-radius: 4px;
+  padding: 0.5em 1em;
+  margin-top: -10px;
+  filter: drop-shadow(0 1px 1px rgba(0, 0, 0, 0.5));
+  background: white;
+  // box-shadow: 0 0 3px rgba(0, 0, 0, 0.5);
+  &::before{
+    content: '';
+    display: block;
+    width: 0px;
+    height: 0px;
+    border: 10px solid red;
+    border-top-color: #333;
+    border-bottom-color: transparent;
+    border-left-color: transparent;
+    border-right-color: transparent;
+    position: absolute;
+    top: 100%;
+    left: 10px;
+  }
+  &::after{
+    content: '';
+    display: block;
+    width: 0px;
+    height: 0px;
+    border: 10px solid red;
+    border-top-color: white;
+    border-bottom-color: transparent;
+    border-left-color: transparent;
+    border-right-color: transparent;
+    position: absolute;
+    top: calc(100% - 2px);
+    left: 10px;
+  }
 }
 </style>
